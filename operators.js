d28a51e6e4ce7ac0410c20d73211932c6ab9901b
Abduvokhidjon <aabduvohid33@gmail.com>
2023-05-19 11:36:01 -0500

    Superhero BG

---
+++ operators.js
@@ -0,0 +1,24 @@
+/*
+in operators.js file
+
+Task 1: Using the logical && operator
+
+You are coding an Superhero battle game, where each superhero has certain skill levels based on the value saved in their score.
+
+Create a variable named superhero and set it your favorite superheroname.
+Create a variable named score and set it to 5
+
+
+Use console.log() that includes superhero, the string " has mid-level skills:" and compares the score variable to above 0 and below 10 using the && operator
+
+The expected output in the console should be: "<superhero name> has mid-level skills: true".
+
+
+*/
+
+var superhero
+superhero = "Naruto"
+var score 
+score = 5
+level = 0<score && score<10
+console.log(superhero + " has mid-level skills: " + level )
